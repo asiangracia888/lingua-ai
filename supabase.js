b4559e37b84efae1dab2691f79ab18f52c7397f8
asiangracia888 <dimacoj239@gmail.com>
2026-09-15 15:54:59 +0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://hxvotkatddmjwermrhll.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_dtTgkiDYE6VNa0xDcXy7_g_Am_jX_mM";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
